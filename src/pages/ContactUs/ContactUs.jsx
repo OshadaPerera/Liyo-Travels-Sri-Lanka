be86f409db0ce924d6c4649cb018d8cfdc251e25
Oshada Perera <oshada.20222343@iit.ac.lk>
2024-07-17 15:48:35 +0530

    contact us content and styling added

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -15,64 +15,85 @@ import {
   faEnvelope,
 } from "@fortawesome/free-solid-svg-icons";
 import "./contactUsStyles.css";
+import useIntersectionObserver from "../../hooks/useIntersectionObserver";
 
 function ContactUs() {
   // Get the current language from the LanguageContext
   const { language } = useContext(LanguageContext);
   const currentContent = contactContent[language];
 
+  // Use the custom hook for each section to observe visibility
+  const contactHeadRef = useIntersectionObserver({ threshold: 0.1 });
+  // const homeChooseRef = useIntersectionObserver({ threshold: 0.1 });
+
   return (
-    <div>
+    <div className="contactBody">
       <NavBar />
       <div className="contact-us">
-        <h1>{currentContent.title}</h1>
-        <p>{currentContent.description}</p>
-        <div className="contact-methods">
-          <a
-            href="https://wa.me/1234567890"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon icon={faWhatsapp} /> {currentContent.whatsapp}
-          </a>
-          <a href="mailto:info@liyotravels.com">
-            <FontAwesomeIcon icon={faEnvelope} /> {currentContent.email}
-          </a>
+        <div className="contactHeadingSection" ref={contactHeadRef}>
+          <h2>{currentContent.title}</h2>
+          <p>{currentContent.description}</p>
+          <div className="contact-methods">
+            <a
+              href="https://wa.me/1234567890"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faWhatsapp} /> {currentContent.whatsapp}
+            </a>
+            <a href="mailto:info@liyotravels.com">
+              <FontAwesomeIcon icon={faEnvelope} />
+              {currentContent.email}
+            </a>
+          </div>
+          <div className="contactAnimation">
+            <dotlottie-player
+              src="https://lottie.host/863758b5-bffe-4f93-ae5f-c0dceed14bcd/aL5OlN0QPf.json"
+              background="transparent"
+              speed="1"
+              loop
+              autoplay
+            ></dotlottie-player>
+          </div>
         </div>
-
         {/* Additional Content */}
         <div className="additional-content">
-          <div className="followus">
-            <h3>{currentContent.followUs}</h3>
-            <a
-              href="https://www.facebook.com/yourpage"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FontAwesomeIcon icon={faFacebook} /> {currentContent.facebook}
-            </a>
-            <a
-              href="https://www.instagram.com/yourpage"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FontAwesomeIcon icon={faInstagram} /> {currentContent.instagram}
-            </a>
-          </div>
-          <div className="address">
-            <h3>{currentContent.addressTitle}</h3>
-            <p>
-              <FontAwesomeIcon icon={faMapMarkerAlt} /> {currentContent.address}
-            </p>
-          </div>
-          {/* <div className="phone">
+          <div className="addressNfollow">
+            <div className="address">
+              <h3>{currentContent.addressTitle}</h3>
+              <p>
+                <FontAwesomeIcon icon={faMapMarkerAlt} />{" "}
+                {currentContent.address}
+              </p>
+            </div>
+            <div className="followus">
+              <h3>{currentContent.followUs}</h3>
+              <div className="followLinks">
+                <a
+                  href="https://www.facebook.com/yourpage"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon icon={faFacebook} className="fbIcon" />
+                </a>
+                <a
+                  href="https://www.instagram.com/yourpage"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon icon={faInstagram} className="instaIcon" />
+                </a>
+              </div>
+            </div>
+
+            {/* <div className="phone">
             <h3>{currentContent.phoneTitle}</h3>
             <p>
               <FontAwesomeIcon icon={faPhone} /> {currentContent.phone}
             </p>
           </div> */}
+          </div>
           <div className="map">
-            <h3>{currentContent.mapTitle}</h3>
             <iframe
               src={currentContent.mapEmbedUrl}
               style={{ border: 0 }}
